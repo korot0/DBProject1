8c79cd33e9173fcadb5a65fe3ac1efa978b971fe
korot0 <108902180+korot0@users.noreply.github.com>
2026-02-27 13:46:33 -0600

    Add todo queries

---
+++ queries.sql
@@ -0,0 +1,9 @@
+-- 2. Given a passenger’s last name and first name and retrieve all trains they are booked on.
+-- 3. Given a day list the passengers traveling on that day with confirmed tickets.
+-- 4. Display the train information (Train Number, Train Name, Source and Destination) and passenger information (Name, Address, Category, ticket status) of passengers who are between the ages of 50 to 60. 
+-- 5. List train name, day and number of passenger on that train.
+-- 6. Enter a train name and retrieve all the passengers with confirmed status traveling on that train.
+-- 7. List passengers that are waitlisted including the name of the train.
+-- 8. List passenger names in descending order that have '605' phone area code.
+-- 9. List name of passengers that are traveling on Thursdays in ascending order.
+
