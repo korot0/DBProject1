8f21d3e7e31b7dd24a267925ad8d2295e9857eb9
Josue Trejo <jt@jt-2.local>
2026-02-27 10:52:30 -0600

    Initial commit

---
+++ import_data.py
@@ -0,0 +1,5 @@
+import sqlite3
+import csv
+
+con = sqlite3.connect("railway.db");
+cur = con.cursor()
---
+++ schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE train (
+  train_number INT PRIMARY KEY,
+  train_name TEXT,
+  premium_fair INT,
+  general_fair INT,
+  source_station TEXT,
+  destination_station TEXT,
+  weekdays TEXT
+);
+
+CREATE TABLE train_status (
+  train_date TEXT,
+  train_name TEXT,
+  premium_seats_available INT,
+  gen_seats_available INT,
+  premium_seats_occupied INT,
+  gen_seats_occupied INT,
+  PRIMARY KEY (train_date, train_name),
+  FOREIGN KEY (train_name) REFERENCES train(train_name)
+);
+
+CREATE TABLE passenger (
+  first_name TEXT,
+  last_name TEXT,
+  address TEXT,
+  city TEXT,
+  county TEXT,
+  phone TEXT,
+  ssn INT PRIMARY KEY,
+  bdate TEXT
+);
+
+
+CREATE TABLE booked (
+  passenger_ssn INT,
+  train_number INT,
+  ticket_type TEXT,
+  status TEXT,
+  PRIMARY KEY (passenger_ssn, train_number),
+  FOREIGN KEY (passenger_ssn) REFERENCES passenger(ssn),
+  FOREIGN KEY (train_number) REFERENCES train(train_number)
+);
